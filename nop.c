dd8b7c3fecc4f4c657606cb7a061526a786763ee
monwabisindlovu <monwabisindlovu78@gmail.com>
2023-09-21 04:30:45 -0700

    nop.c

---
+++ nop.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+/**
+ * nop - Doesn't do anything.
+ * @stack: Double pointer to the head of the stack.
+ * @line_number: The line number currently being run.
+ *
+ * Description: This function is designed to handle the 'nop' opcode.
+ * The 'nop' opcode doesn't do anything.
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+(void)stack;
+(void)line_number;
+}
+
